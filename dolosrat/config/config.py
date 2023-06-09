b5a8785ee06b118ec41350631cd9e363c8eee98a
Ryan Instrell <ryaninstrell@hotmail.co.uk>
2023-06-09 21:25:26 +0100

    Create initial configuration structure.

---
+++ dolosrat/config/config.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Creator : Ryan I.
+# Created Date: 07/06/2023
+# version ='1.0'
+# ---------------------------------------------------------------------------
+"""
+Houses the primary configuration for DolosRAT. 
+Provides a class wherein child configs can inherit and
+provide functionality-specific configurations.
+"""
