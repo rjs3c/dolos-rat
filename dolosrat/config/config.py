4a0e37b2ac070d836672aea4126aec424c1c2e88
Ryan Instrell <ryaninstrell@hotmail.co.uk>
2023-06-09 21:36:18 +0100

    Implemented 'Config' methods and logic.

--- dolosrat/config/config.py
+++ dolosrat/config/config.py
@@ -13,7 +13,7 @@ provide functionality-specific configurations.
 
 # Built-in/Generic Imports.
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Generator
 
 @dataclass
 class Config: 
@@ -23,5 +23,36 @@ class Config:
     neccessary.
     """
 
-    """ ... """
+    """ Stores functionality-specific config (any type). """
     _conf: Any
+
+    @property
+    def conf(self: object) -> Any: 
+        """Getter method for _conf.
+
+        May be overridden by inheriting config
+        classes for returning differing types.
+
+        Returns:
+            Any: _conf of any type.
+        """
+        return self._conf
+
+    # Permits the iteration of Configuration, or inherited, 
+    # object.
+    def __iter__(self: object) -> Generator[Any, Any, Any]: 
+        """Enables iteration of a Config derivative.
+
+        Yields:
+            Generator[Any, Any, Any]: Iterable of _config attribute.
+        """
+        yield from self._conf
+
+    def __str__(self: object) -> str: 
+        """Friendly string representation of class.
+
+        Returns:
+            str: Friendly string of class for 
+            later, more trivial comparison.
+        """
+        return f'{type(self.__name__)}'
