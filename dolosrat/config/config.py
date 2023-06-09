869017189b8bb8eecca6b17c6989f02d6d61c7a1
Ryan Instrell <ryaninstrell@hotmail.co.uk>
2023-06-09 21:28:56 +0100

    Create 'Config' dataclass, add '_conf' attribute.

--- dolosrat/config/config.py
+++ dolosrat/config/config.py
@@ -10,3 +10,18 @@ Houses the primary configuration for DolosRAT.
 Provides a class wherein child configs can inherit and
 provide functionality-specific configurations.
 """
+
+# Built-in/Generic Imports.
+from dataclasses import dataclass
+from typing import Any
+
+@dataclass
+class Config: 
+    """Stores application and functionality-specific
+    config. Specific configurations shall inherit from this
+    base class, overriding _conf, and other functions, as 
+    neccessary.
+    """
+
+    """ ... """
+    _conf: Any
