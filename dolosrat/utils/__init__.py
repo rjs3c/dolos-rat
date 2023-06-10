4f64570fbcb5a10a397fc2d28516a8f963a2b186
Ryan Instrell <ryaninstrell@hotmail.co.uk>
2023-06-10 20:48:55 +0100

    Populate 'logger.py' with LoggerWrapper.

---
+++ dolosrat/utils/__init__.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Creator : Ryan I.
+# Created Date: 06/06/2023
+# version ='1.0'
+# ---------------------------------------------------------------------------
+"""
+DolosRAT provides a GUI-based RAT client and server, purposed for demonstrating
+techniques frequently used within scammer take-down operations. Please note that
+the use of this tool is for educational purposes only.
+"""
+
+from . import logger
---
+++ dolosrat/utils/logger.py
@@ -0,0 +1,35 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Creator : Ryan I.
+# Created Date: 07/06/2023
+# version ='1.0'
+# ---------------------------------------------------------------------------
+"""
+Provides functionality to supplement application
+logging and auditing.
+"""
+
+# Built-in/Generic Imports.
+from typing import Union
+from os import PathLike
+from pathlib import Path
+
+class LoggerWrapper:
+    """Provides wrapper for easily
+    interfacing with 'logger' library.
+    """
+
+    # Comprises path for 'logs' directory.
+    _LOG_PATH: Union[str, PathLike] = Path("../logs")
+
+    def __init__(self: object) -> None:
+        """Initialises LoggerWrapper class.
+        """
+        ...
+
+    def __del__(self: object) -> None:
+        """Destroy logging handles and
+        release resources.
+        """
+        ...
