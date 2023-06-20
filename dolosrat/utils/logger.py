6418fa0b35c82f02dbed04d7b17ad204c51ebdd1
Ryan Instrell <ryaninstrell@hotmail.co.uk>
2023-06-20 10:24:53 +0100

    Comment-out redundant constant.

--- dolosrat/utils/logger.py
+++ dolosrat/utils/logger.py
@@ -55,7 +55,7 @@ class LoggerWrapper(BaseWrapper):
     """
 
     # Comprises path for 'logs' directory.
-    _LOG_PATH: Union[str, PathLike] = Path("../logs")
+    # _LOG_PATH: Union[str, PathLike] = Path("../logs")
 
     def __init__(
         self: object,
