39f20acdac47d4ada1a48cc80c75d96fdf961d9a
Ryan Instrell <ryaninstrell@hotmail.co.uk>
2023-06-20 08:43:45 +0100

    Implement BaseWrapper.

---
+++ dolosrat/utils/wrapper.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Creator : Ryan I.
+# Created Date: 20/06/2023
+# version ='1.0'
+# ---------------------------------------------------------------------------
+"""
+...
+"""
+
+class BaseWrapper:
+    """_summary_
+    """
+    def __init__(self: object) -> None:
+        """_summary_
+
+        Args:
+            self (object): _description_
+        """
+        pass
