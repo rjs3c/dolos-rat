ca722ba770c864f68cdfba50eb902020c311e855
Ryan Instrell <ryaninstrell@hotmail.co.uk>
2023-06-22 10:48:19 +0100

    Implement timing out of listening.

--- dolosrat/utils/net/server.py
+++ dolosrat/utils/net/server.py
@@ -13,14 +13,13 @@
 from socketserver import BaseRequestHandler, TCPServer, BaseServer
 from ipaddress import IPv4Address
 from typing import Any
-# from threading import Timer # https://stackoverflow.com/questions/2697039/python-equivalent-of-setinterval
 
 # Modules.
-from utils.misc.wrapper import BaseWrapper
-from utils.misc.logger import get_logger, LoggerWrapper, LoggerLevel
+from utils.misc.wrapper import BaseWrapper # pylint: disable=import-error
+from utils.misc.logger import get_logger, LoggerWrapper, LoggerLevel # pylint: disable=import-error
+from config.network import NetworkConfig, get_network_conf # pylint: disable=import-error
+from config.logger import get_logger_conf # pylint: disable=import-error
 from .interface import Ifa, IPv4Host
-from config.network import NetworkConfig, get_network_conf
-from config.logger import get_logger_conf
 
 class SingleThreadedTCPHandler(BaseRequestHandler):
     """_summary_
@@ -35,7 +34,6 @@ class SingleThreadedTCPHandler(BaseRequestHandler):
         """_summary_
 
         Args:
-            self (object): _description_
             request (Any): _description_
             client_address (Any): _description_
             server (BaseServer): _description_
@@ -53,13 +51,15 @@ class SingleThreadedTCPHandler(BaseRequestHandler):
         """_summary_
         """
 
-        print(self.__dict__)
+        # Increment request counter
+        # for timeout threshold.
+        self.server.inc_req()
 
 class SingleThreadedTCPServer(TCPServer):
     """_summary_
     """
 
-    timeout = 5
+    timeout: int = 30
 
     def __init__(self: object, server_address, RequestHandlerClass) -> None:
         """_summary_
@@ -75,9 +75,51 @@ class SingleThreadedTCPServer(TCPServer):
 
         TCPServer.__init__(self, server_address, RequestHandlerClass)
 
+        # Allow binding to same address after closing.
+        self.allow_reuse_address = True
+
+        # Counters used to identify if no requests
+        # have been receieved within interval.
+        self._req_ctr: int = 0
+        self._req_thres: int = 0
+
+    def timeout_check(self: object) -> bool:
+        """_summary_
+
+        Args:
+            self (obejct): _description_
+
+        Returns:
+            bool: _description_
+        """
+
+        return self._req_thres == self._req_ctr
+
+    def inc_thres(self: object) -> None:
+        """_summary_
+
+        Args:
+            self (object): _description_
+        """
+
+        self._req_thres += 1
+
+    def inc_req(self: object) -> None:
+        """_summary_
+
+        Args:
+            self (object): _description_
+
+        Returns:
+            _type_: _description_
+        """
+
+        self._req_ctr += 1
+
 class TCPServerWrapper(BaseWrapper):
     """_summary_
     """
+
     def __init__(
         self: object,
         network_config: NetworkConfig,
@@ -101,11 +143,6 @@ class TCPServerWrapper(BaseWrapper):
         # from selected host.
         self._port = self.config.conf['selected_host'].port
 
-        # Captures a state by which listening
-        # is conducted. Used to check if timed out.
-        # By default, stopped.
-        self._listen_stopped: bool = True
-
         # Registers handle for logger.
         self._register_handle(logger)
 
@@ -128,44 +165,57 @@ class TCPServerWrapper(BaseWrapper):
             )
         )
 
-    def _inv_listen_state(self: object) -> None:
+    def _close(self: object) -> None:
         """_summary_
 
         Args:
             self (object): _description_
         """
-        self._listen_stopped = not self._listen_stopped
 
-    def shutdown(self: object) -> None:
-        """_summary_
+        # Write log to inform of shutdown.
+        self._get_handle('LoggerWrapper').write_log(
+                f'Server on \'{self._host}:{self._port}\' shutting down.',
+                LoggerLevel.INFO
+        )
 
-        Args:
-            self (object): _description_
-        """
-        ...
+        # Forcefully shuts down server and
+        # releases resources.
+        self._get_handle( # pylint: disable=protected-access
+            'SingleThreadedTCPServer' # pylint: disable=protected-access
+        )._BaseServer__shutdown_request = True # pylint: disable=protected-access
 
     def listen(self: object) -> None:
         """_summary_
         """
 
-        self._inv_listen_state()
-
+        # Generate status log that listening has started.
         self._get_handle('LoggerWrapper').write_log(
                 f'Server listening on \'{self._host}:{self._port}\'...',
                 LoggerLevel.INFO
         )
 
+        # Processing one request at a time
+        # within a controlled loop.
         # Poll interval: 0.5s.
-        # Timeout: 5s.
-        # while self._listen_stopped is not True:
-        #     self._get_handle('SingleThreadedTCPServer').handle_request()
-        #     print("hello")
+        # Timeout: 30s.
+        while self._get_handle('SingleThreadedTCPServer').timeout_check():
+            # Increment threshold counter for timeout.
+            self._get_handle('SingleThreadedTCPServer').inc_thres()
 
-        # self._get_handle('SingleThreadedTCPServer')._BaseServer__shutdown_request = True
+            # handle_request() used instead of serve_forever()
+            # as to avoid single-thread deadlocking issues.
+            # This is the case as we want to timeout listening
+            # after period of inactivity.
+            self._get_handle('SingleThreadedTCPServer').handle_request()
 
-        self._get_handle('SingleThreadedTCPServer').serve_forever()
+        # Write log to warn of timing out.
+        self._get_handle('LoggerWrapper').write_log(
+                f'Idle server on \'{self._host}:{self._port}\' timing out after 30s.',
+                LoggerLevel.WARNING
+        )
 
-        self._get_handle('SingleThreadedTCPServer').server_close()
+        # Shutdown server, NOT connection.
+        self._close()
 
 def get_tcp_server_wrapper(network_config: NetworkConfig) -> TCPServerWrapper:
     """_summary_
