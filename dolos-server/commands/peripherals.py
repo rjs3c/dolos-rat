889accf021bd1648f5e1128eb94c4b82abbab832
Ryan Instrell <ryaninstrell@hotmail.co.uk>
2023-07-26 22:50:20 +0100

    Structural changes.

--- dolos-server/commands/peripherals.py
+++ dolos-server/commands/peripherals.py
@@ -10,3 +10,40 @@ DolosRAT provides a GUI-based RAT client and server, purposed for demonstrating
 techniques frequently used within scammer take-down operations. Please note that
 the use of this tool is for educational purposes only.
 """
+
+# Built-in/Generic Imports.
+from typing import Any
+
+# Modules.
+from .command import Command
+
+class KeystrokeLogCommand(Command):
+    """_summary_
+
+    Args:
+        Command (_type_): _description_
+    """
+
+    def __init__(self: object) -> Any:
+        """_summary_
+
+        Args:
+            self (object): _description_
+
+        Returns:
+            Any: _description_
+        """
+
+        ...
+
+    def execute(self: object) -> Any:
+        """_summary_
+
+        Args:
+            self (object): _description_
+
+        Returns:
+            Any: _description_
+        """
+
+        ...
